55a3a0358a67bfa501b6f86fff16fd7b0c1c920e
spobb <gverlaeken@gmail.com>
2024-04-18 15:37:37 +0200

    added connection to db

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$db   = 'records4sale';
+$user = 'records4sale';
+$pass = 'nn9zy.)s]9IGl@]c';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $options);
