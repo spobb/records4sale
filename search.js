c93b8292879b65b079f9976c7a78a89057436d38
spobb <gverlaeken@gmail.com>
2024-05-03 19:10:46 +0200

    corrected variable name

--- search.js
+++ search.js
@@ -1,14 +1,14 @@
 document.getElementById('search').addEventListener('input', e => {
     srch = e.target.value.toLowerCase();
-    itm = document.querySelectorAll('.item-card');
+    itms = document.querySelectorAll('.item-card');
     
-    console.log(srch);
-    itm.forEach(itms => {
-        titles = itms.childNodes[1].childNodes[3].innerText.toLowerCase();
-        artists = itms.childNodes[1].childNodes[1].innerText.toLowerCase();
+    itms.forEach(itm => {
+        (console.log(itms));
+        titles = itm.childNodes[1].childNodes[3].innerText.toLowerCase();
+        artists = itm.childNodes[1].childNodes[1].innerText.toLowerCase();
 
         if(!titles.includes(srch) && !artists.includes(srch)) {
-            itms.classList.add('hidden');
-        } else itms.classList.remove('hidden');
+            itm.classList.add('hidden');
+        } else itm.classList.remove('hidden');
     });
 })
