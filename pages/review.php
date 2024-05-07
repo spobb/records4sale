716ade067524b49e95296f8803285be74f6cd746
spobb <gverlaeken@gmail.com>
2024-05-07 14:46:30 +0200

    made review page more lightweight

--- pages/review.php
+++ pages/review.php
@@ -1,19 +1,23 @@
-<?php
-$sql = "SELECT i.id, i.label, i.price, c.label as category, g.label as genre, a.label as artist 
-        FROM item i
-        LEFT JOIN category c ON c.id = i.category_id 
-        LEFT JOIN genre g ON g.id = i.genre_id 
-        LEFT JOIN artist a ON a.id = i.artist_id";
-
-$statement = $pdo->query($sql);
-$results = $statement->fetchAll();
-?>
-$row['label']
-
 <h1>Review of:</h1>
 <section>
     <div class="flex-wrapper">
+        <h2><?= $_REQUEST['artist']; ?></h2>
+        <p><?= $_REQUEST['label'];  ?></p>
+
         <form action="" method="POST">
+            <div class="review-stars">
+                <input type="radio" name="stars" id="stars-1">
+                <label for="stars-1"></label>
+                <input type="radio" name="stars" id="stars-2">
+                <label for="stars-2"></label>
+                <input type="radio" name="stars" id="stars-3">
+                <label for="stars-3"></label>
+                <input type="radio" name="stars" id="stars-4">
+                <label for="stars-4"></label>
+                <input type="radio" name="stars" id="stars-5">
+                <label for="stars-5"></label>
+            </div>
+
             <label for="message"></label>
             <textarea name="message" cols="30" rows="10" placeholder="Write a review..."></textarea>
 
