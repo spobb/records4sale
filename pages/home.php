f3e7ec615dcd9233069ade40a2cb8a44524c637a
spobb <gverlaeken@gmail.com>
2024-05-08 17:42:32 +0200

    added text logo as image

--- pages/home.php
+++ pages/home.php
@@ -3,34 +3,43 @@ require 'connection.php';
 require 'include.php';
 
 if (isset($_REQUEST['action'])) {
-    if ($_REQUEST['action'] == 'search') {
-        $sql = sprintf(
-            "SELECT
-                i.id,
-                (
-                SELECT
-                    i.label
-                WHERE
-                    i.label LIKE '%%%s%%'
-            ) AS label,
-            i.price,
-            i.runtime,
-            c.label AS category,
-            g.label AS genre,
-            a.label AS artist
-            FROM
-                item i
-            LEFT JOIN category c ON
-                c.id = i.category_id
-            LEFT JOIN genre g ON
-                g.id = i.genre_id
-            LEFT JOIN artist a ON
-                a.id = i.artist_id AND i.label LIKE '%%%s%%'",
-            $_REQUEST['search'],
-            $_REQUEST['search']
-        );
-    } else {
-        echo 'no action specified!';
+    switch ($_REQUEST['action']) {
+        case 'search':
+            $sql = sprintf(
+                "SELECT
+                    i.id,
+                    (
+                    SELECT
+                        i.label
+                    WHERE
+                        i.label LIKE '%%%s%%'
+                ) AS label,
+                i.price,
+                i.runtime,
+                c.label AS category,
+                g.label AS genre,
+                a.label AS artist
+                FROM
+                    item i
+                LEFT JOIN category c ON
+                    c.id = i.category_id
+                LEFT JOIN genre g ON
+                    g.id = i.genre_id
+                LEFT JOIN artist a ON
+                    a.id = i.artist_id AND i.label LIKE '%%%s%%'",
+                $_REQUEST['search'],
+                $_REQUEST['search']
+            );
+            break;
+        case 'register':
+            echo 'registered!';
+            break;
+        case 'identify':
+            echo 'identified';
+            break;
+        default:
+            echo 'no action specified!';
+            break;
     }
 } else {
     $sql = sprintf(
@@ -47,12 +56,14 @@ $results = $statement->fetchAll();
 
 ?>
 
-<h1>Our records</h1>
-<section class="item-catalog">
-    <?php
-    foreach ($results as $row) {
-        if ($row['label'])
-            include 'item-card.php';
-    }
-    ?>
-</section>
+<main>
+    <h1>Our records</h1>
+    <section class="item-catalog">
+        <?php
+        foreach ($results as $row) {
+            if ($row['label'])
+                include 'item-card.php';
+        }
+        ?>
+    </section>
+</main>
