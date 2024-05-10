9e4d410a520d3298a0bed282bf6597427356f48e
spobb <gverlaeken@gmail.com>
2024-05-10 13:47:12 +0200

    changed button to form for review in item card

--- pages/item-card.php
+++ pages/item-card.php
@@ -1,7 +1,7 @@
 <article class="item-card">
     <div>
         <a href="#" class="artist"><?= $row['artist'] ?></a>
-        <h2 class="title"><?= $row['label'] ?></h2>
+        <a href="index.php?page=item&id=<?= $row['id'] ?>" class="title"><?= $row['label'] ?></a>
     </div>
     <img src="<?= album_cover($row) ?> " alt="album cover">
     <!-- <p><?= $row['release'] ?></p> -->
@@ -12,7 +12,7 @@
             $row['genre']
             ?></p>
         <footer>
-            <button>Review</button>
+            <form action="index.php?page=item&custom=review&action=add" method="get"><button>Review</button></form>
             <span class="price"><?= $row['price'] . '' ?></span>
             <button>Buy</button>
         </footer>
