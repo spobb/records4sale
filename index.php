3a71bd9218b6a16bec2e4c922caebb8419c481f2
spobb <gverlaeken@gmail.com>
2024-05-13 02:23:32 +0200

    made all pages php

--- index.php
+++ index.php
@@ -2,23 +2,6 @@
 $page = 'home';
 
 if (isset($_GET['page'])) {
-    switch ($_GET['page']) {
-        case 'profile':
-            $page = 'pages/profile.php';
-            break;
-        case 'home':
-            $page = 'pages/home.php';
-            break;
-        case 'item':
-            $page = 'pages/item.php';
-            break;
-        case 'search':
-            $page = 'pages/search.php';
-            break;
-        default:
-            $page = 'pages/' . $_GET['page'] . '.html';
-            break;
-    }
+    $page = 'pages/' . $_GET['page'] . '.php';
 }
-
 include 'pages/skeleton.php';
