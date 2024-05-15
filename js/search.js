06cf256d0f5e92eb9ee7a77e01d6807702159f74
spobb <gverlaeken@gmail.com>
2024-05-15 18:28:16 +0200

    fixed search script

--- js/search.js
+++ js/search.js
@@ -1,16 +1,15 @@
-$('#search').on('input', e => {
+$('#search').on('input', () => {
     let input = $('#search')
     .val()
     .toLowerCase()
     .replace('/[-,;:!\'\"\\]/', '');
 
-    const itms = $('.item-card').toArray(); 
-
-    itms.forEach(itm => {
-        let titles = itm.childNodes[1].childNodes[3].innerText.toLowerCase();
-        let artists = itm.childNodes[1].childNodes[1].innerText.toLowerCase();
-        
-        !titles.includes(input) && !artists.includes(input) ? itm.classList.add('hidden') : itm.classList.remove('hidden');
+    const itms = $('.item-card').toArray();
+    
+    itms.forEach(function (itm) {
+        title = itm.querySelector('.title').innerText.toLowerCase();
+        artist = itm.querySelector('.artist').innerText.toLowerCase();
+        !title.includes(input) && !artist.includes(input) ? itm.classList.add('hidden') : itm.classList.remove('hidden');
     });
 })
 
