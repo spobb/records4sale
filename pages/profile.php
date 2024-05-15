666400e0c8a7ac6ab9d0c6bcc8e3e14b9146c389
spobb <gverlaeken@gmail.com>
2024-05-15 19:57:41 +0200

    started new profile page

--- pages/profile.php
+++ pages/profile.php
@@ -1,43 +1,30 @@
-<?php include 'connection.php';
-$sql = "SELECT item_id, rating, comment, i.label as item FROM review 
-LEFT JOIN item i on i.id = item_id 
-WHERE user_id = 1";
-$statement = $pdo->query($sql);
-$reviews = $statement->fetchAll();
-$c = 0;
-
-foreach ($reviews as $row) {
-    ++$c;
-}
-?>
-<main class="profile-page">
-    <nav class="sidebar">
-        <a href="#profile-information">Your profile</a>
-        <a href="#profile-favorites">Your favorites</a>
-        <a href="#">Blah blah</a>
-        <a href="#profile-reviews">Your reviews (<?php echo $c; ?>)</a>
-        <a href="#">Blah blah</a>
-        <a href="#">Blah blah</a>
-        <a href="#profile-security">Security</a>
-    </nav>
+<main>
+    <div class="profile-hero wrapper">
+        <h1>Hello, John Doe</h1>
     </div>
-    <div>
-        <h1 id="profile-information">Hello, Jean Pierre</h1>
-        <h2 id="profile-favorites">Your favorites</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus beatae illo voluptas quam saepe quos tempore ad repudiandae minus fugit?</p>
-        <h2>Lorem, ipsum dolor.</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rem architecto consequuntur veritatis ullam hic eveniet voluptates dolores corrupti dolorem! Ea vel, esse quasi alias veniam vitae inventore! Pariatur, possimus.</p>
-        <h2>Lorem, ipsum dolor.</h2>
-        <h2 id="profile-reviews">Your reviews</h2>
-        <div class="profile-reviews">
-            <?php foreach ($reviews as $row) {
-                include 'partials/profile-review.php';
-            } ?>
+
+    <h1>Your reviews</h1>
+    <div class="profile-reviews">
+        <div class="review column-wrapper">
+            <h2>Title</h2>
+            <span>Rating</span>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, voluptate. Eligendi debitis earum quisquam aliquid quis,
+                a cupiditate ratione corporis dolorum maiores deserunt quidem quia quam voluptatibus provident neque cum qui!
+                Aut necessitatibus, saepe voluptatum quam odio ex, nisi, aliquid soluta quidem dolore beatae vero neque ipsam aspernatur facere? Hic!</p>
         </div>
-        <h2>Lorem, ipsum dolor.</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rem architecto consequuntur veritatis ullam hic eveniet voluptates dolores corrupti dolorem! Ea vel, esse quasi alias veniam vitae inventore! Pariatur, possimus.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rem architecto consequuntur veritatis ullam hic eveniet voluptates dolores corrupti dolorem! Ea vel, esse quasi alias veniam vitae inventore! Pariatur, possimus.</p>
-        <h2 id="profile-security">Security</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam voluptatibus sunt, earum praesentium accusamus similique sint iste unde nemo quos dolores magni veniam! Repellat quibusdam possimus reprehenderit, quia veniam
-            blanditiis minima? Harum debitis, natus numquam velit doloremque consequuntur, optio deleniti necessitatibus voluptatum reprehenderit beatae cupiditate corrupti perspiciatis eaque delectus modi!</p>
+        <div class="review column-wrapper">
+            <h2>Title</h2>
+            <span>Rating</span>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, quo.</p>
+        </div>
+        <div class="review column-wrapper">
+            <h2>Title</h2>
+            <span>Rating</span>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam qui labore necessitatibus maxime quas.
+                Ipsam accusantium assumenda quo dignissimos ipsa?</p>
+        </div>
+        <span>Add a review</span>
+    </div>
+
+    <h1>Personal information</h1>
 </main>
