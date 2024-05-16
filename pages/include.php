b98d93b5995a94031440723c41bd61576a2b1a34
spobb <gverlaeken@gmail.com>
2024-05-16 11:07:48 +0200

    forgot assets folder

--- pages/include.php
+++ pages/include.php
@@ -4,7 +4,7 @@
 
 function album_cover($result)
 {
-    $ret = sprintf('./public/img/covers/%s-%s.jpg', $result['artist'], $result['label']);
+    $ret = sprintf('./public/assets/%s-%s.jpg', $result['artist'], $result['label']);
     $ret = str_replace(' ', '-', $ret);
     $ret = preg_replace('/[:]/', '', $ret);
     return strtolower($ret);
