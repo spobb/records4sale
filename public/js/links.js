da0ad98c0d72f95845907b74ed548817920555a2
spobb <gverlaeken@gmail.com>
2024-05-19 22:54:10 +0200

    random colors script

---
+++ public/js/links.js
@@ -0,0 +1,12 @@
+const colors = ['#ca4246',
+                '#e16541',
+                '#f18f43',
+                '#8b9862',
+                '#476098',
+                '#a7489b']
+i = 0;
+
+$('.artist').each(function() {
+    $(this).css('color', colors[i % colors.length]);
+    ++i;
+});
