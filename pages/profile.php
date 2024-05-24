e3a9bf2c0636c2a1de2c8a3907773556ffd64c43
spobb <gverlaeken@gmail.com>
2024-05-24 21:50:28 +0200

    profile nav tabs

--- pages/profile.php
+++ pages/profile.php
@@ -8,72 +8,123 @@
         <div class="review column-wrapper">
             <h2 class="overflow">A valediction</h2>
             <span>4.5/5</span>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, voluptate. Eligendi debitis earum quisquam aliquid quis,
-                a cupiditate ratione corporis dolorum maiores deserunt quidem quia quam voluptatibus provident neque cum...</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, voluptate. Eligendi debitis earum
+                quisquam aliquid quis,
+                a cupiditate ratione corporis dolorum maiores deserunt quidem quia quam voluptatibus provident neque
+                cum...</p>
         </div>
         <div class="review column-wrapper">
             <h2 class="overflow">A valediction</h2>
             <span>4.5/5</span>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, voluptate. Eligendi debitis earum quisquam aliquid quis,
-                a cupiditate ratione corporis dolorum maiores deserunt quidem quia quam voluptatibus provident neque cum...</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, voluptate. Eligendi debitis earum
+                quisquam aliquid quis,
+                a cupiditate ratione corporis dolorum maiores deserunt quidem quia quam voluptatibus provident neque
+                cum...</p>
         </div>
         <div class="review column-wrapper">
             <h2 class="overflow">A valediction</h2>
             <span>4.5/5</span>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, voluptate. Eligendi debitis earum quisquam aliquid quis,
-                a cupiditate ratione corporis dolorum maiores deserunt quidem quia quam voluptatibus provident neque cum...</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, voluptate. Eligendi debitis earum
+                quisquam aliquid quis,
+                a cupiditate ratione corporis dolorum maiores deserunt quidem quia quam voluptatibus provident neque
+                cum...</p>
         </div>
         <div class="review column-wrapper">
             <h2 class="overflow">A valediction</h2>
             <span>4.5/5</span>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, voluptate. Eligendi debitis earum quisquam aliquid quis,
-                a cupiditate ratione corporis dolorum maiores deserunt quidem quia quam voluptatibus provident neque cum...</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, voluptate. Eligendi debitis earum
+                quisquam aliquid quis,
+                a cupiditate ratione corporis dolorum maiores deserunt quidem quia quam voluptatibus provident neque
+                cum...</p>
         </div>
         <div class="review column-wrapper">
             <h2 class="overflow">A valediction</h2>
             <span>4.5/5</span>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, voluptate. Eligendi debitis earum quisquam aliquid quis,
-                a cupiditate ratione corporis dolorum maiores deserunt quidem quia quam voluptatibus provident neque cum...</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, voluptate. Eligendi debitis earum
+                quisquam aliquid quis,
+                a cupiditate ratione corporis dolorum maiores deserunt quidem quia quam voluptatibus provident neque
+                cum...</p>
         </div>
         <span>Add a review</span>
     </div>
 
-    <nav class="profile-nav wrapper">
+    <hr>
 
-        <label for="profile-contact"><input type="radio" name="nav" id="profile-contact" checked>
-            Contact
-        </label>
+    <div class="tabs">
+        <nav class="profile-nav wrapper">
 
-        <label for="profile-about">
-            <input type="radio" name="nav" id="profile-about">
-            About
-        </label>
+            <!-- <label for="profile-contact"><input type="radio" name="nav" id="profile-contact" class="tab=button"
+                    data-id="tab1" checked>
+                Contact
+            </label>
+    
+            <label for="profile-about">
+                <input type="radio" name="nav" id="profile-about" class="tab=button" data-id="tab2">
+                About
+            </label>
+    
+            <label for="profile-safety"><input type="radio" name="nav" id="profile-safety" class="tab=button"
+                    data-id="tab3">
+                Safety
+            </label> -->
 
-        <label for="profile-safety"><input type="radio" name="nav" id="profile-safety">
-            Safety
-        </label>
+            <button class="tab-button active" data-id="tab1">Contact</button>
+            <button class="tab-button" data-id="tab2">About</button>
+            <button class="tab-button" data-id="tab3">Safety</button>
+            <button class="tab-button" data-id="tab4">test</button>
+            <button class="tab-button" data-id="tab5">test</button>
 
-    </nav>
-    <div class="profile-tab wrapper">
-        <div class="address column-wrapper">
-            <span>Phone</span>
-            <span>E-mail</span>
-            <span>Address</span>
+        </nav>
+        <div class="profile-tab wrapper active" id="tab1">
+            <div class="address column-wrapper">
+                <span>Phone</span>
+                <span>E-mail</span>
+                <span>Address</span>
+            </div>
+            <div class="column-wrapper">
+                <span>+32 485 58 45 75</span>
+                <span>gverlaeken@student.efp.be</span>
+                <span>Rue de Stalle, 292B 1180 Bruxelles</span>
+            </div>
         </div>
-        <div class="column-wrapper">
-            <span>+32 485 58 45 75</span>
-            <span>gverlaeken@student.efp.be</span>
-            <span>Rue de Stalle, 292B 1180 Bruxelles</span>
+        <div class="profile-tab wrapper" id="tab2">
+            <div class="address column-wrapper">
+                <span>First name</span>
+                <span>Last name</span>
+            </div>
+            <div class="column-wrapper">
+                <span>Guillaume</span>
+                <span>Verlaeken</span>
+            </div>
         </div>
-    </div>
-    <div class="profile-tab wrapper">
-        <div class="address column-wrapper">
-            <span>First name</span>
-            <span>Last name</span>
+        <div class="profile-tab wrapper" id="tab3">
+            <div class="address column-wrapper">
+                <span>TAB 3</span>
+            </div>
+            <div class="column-wrapper">
+                <span>3</span>
+            </div>
         </div>
-        <div class="column-wrapper">
-            <span>Guillaume</span>
-            <span>Verlaeken</span>
+        <div class="profile-tab wrapper" id="tab4">
+            <div class="address column-wrapper">
+                <span>First name</span>
+                <span>Last name</span>
+            </div>
+            <div class="column-wrapper">
+                <span>Guillaume</span>
+                <span>Verlaeken</span>
+            </div>
+        </div>
+        <div class="profile-tab wrapper" id="tab5">
+            <div class="address column-wrapper">
+                <span>First name</span>
+                <span>Last name</span>
+            </div>
+            <div class="column-wrapper">
+                <span>Guillaume</span>
+                <span>Verlaeken</span>
+            </div>
         </div>
     </div>
 </main>
+<script src="/records4sale/public/js/tabs.js"></script>
---
+++ public/js/tabs.js
@@ -0,0 +1,20 @@
+const tabs = document.querySelector('.profile-nav');
+const tabButton = document.querySelectorAll('.tab-button');
+const tab = document.querySelectorAll('.profile-tab');
+
+tabs.addEventListener('click', (e) => {
+	const id = e.target.dataset.id;
+	if (id) {
+		tabButton.forEach((b) => {
+			b.classList.remove('active');
+		});
+		e.target.classList.add('active');
+		console.log('active');
+
+		tab.forEach((t) => {
+			t.classList.remove('active');
+		});
+		const elem = document.getElementById(id);
+		elem.classList.add('active');
+	}
+});
