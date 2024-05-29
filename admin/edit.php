b4abec776dc620c131693f130b046f3aaa3545bb
spobb <gverlaeken@gmail.com>
2024-05-29 18:16:58 +0200

    backend changes

--- admin/edit.php
+++ admin/edit.php
@@ -19,14 +19,16 @@ if (isset($_GET['id'])) {
 if ($_GET['type'] == 'item') {
     require 'edit_item.php';
 } else {
-    ?>
+?>
     <form action="save.php" method="POST" class="form">
         <label for="type">Type</label>
         <input type="text" name="type" value="<?= $_GET['type']; ?>" readonly>
-        <label for="id">ID</label>
-        <input type="text" name="id" value="<?= $record['id']; ?>" readonly>
+
+        <input type="hidden" name="id" value="<?= $record['id']; ?>" readonly>
+
         <label for="label">Label</label>
         <input type="text" name="label" value="<?= $record['label']; ?>">
+
         <button class="button">Save</button>
     </form>
     <a href="index.php?page=listing" class="button">Cancel</a>
